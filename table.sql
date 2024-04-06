1d62b3b9a94a1eb5988b2fd4102691f550d7dd4f
Soojin Jun <157337929+corvina1208@users.noreply.github.com>
2024-04-06 22:43:00 +0900

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,5 +1,6 @@
-```sql
+```
 SELECT table_name, tablespace_name
   FROM user_tables;
   WHERE table_name = upper('&table_name');
 ```
+![image](https://github.com/corvina1208/Scripts/assets/157337929/bc86f401-5c46-4924-a836-d034ac36d39f)
